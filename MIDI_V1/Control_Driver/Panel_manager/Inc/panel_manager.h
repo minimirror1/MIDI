ee13a68c3b43955e669276d1a344e5c7685bfeba
shs <minimirror1@gmail.com>
2021-03-23 16:28:19 +0900

    c,h 생성

---
+++ MIDI_V1/Control_Driver/Panel_manager/Inc/panel_manager.h
@@ -0,0 +1,13 @@
+/*
+ * panel_manager.h
+ *
+ *  Created on: 2021. 3. 23.
+ *      Author: shin
+ */
+
+#ifndef PANEL_MANAGER_INC_PANEL_MANAGER_H_
+#define PANEL_MANAGER_INC_PANEL_MANAGER_H_
+
+
+
+#endif /* PANEL_MANAGER_INC_PANEL_MANAGER_H_ */
---
+++ MIDI_V1/Control_Driver/Panel_manager/Src/panel_manager.c
@@ -0,0 +1,12 @@
+/*
+ * panel_manager.c
+ *
+ *  Created on: 2021. 3. 23.
+ *      Author: shin
+ */
+
+
+#include "main.h"
+#include "panel_manager.h"
+
+
